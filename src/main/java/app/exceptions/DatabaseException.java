813427ebeba9e5d7d6686ebca6d6ce2dea2cd519
olivermjmj <olivermjmj@outlook.dk>
2025-10-21 15:52:36 +0200

    added hashing to pom.

--- src/main/java/app/exceptions/DatabaseException.java
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -1,7 +1,17 @@
 package app.exceptions;
 
-public class DatabaseException extends RuntimeException {
-    public DatabaseException(String message) {
-        super(message);
+public class DatabaseException extends Exception
+{
+    public DatabaseException(String userMessage)
+    {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+    }
+
+    public DatabaseException(String userMessage, String systemMessage)
+    {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+        System.out.println("errorMessage: " + systemMessage);
     }
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,4 +1,34 @@
 package app.persistence;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserMapper {
+
+    public static void saveUser(String userEmail, String username, String passwordHash) throws DatabaseException {
+
+        String sql = "INSERT INTO users (userEmail, name, password, role, balance) VALUES (?, ?, ?, ?, ?)";
+
+        try(Connection connection = ConnectionPool.getInstance().getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            stmt.setString(1, userEmail);
+            stmt.setString(2, username);
+            stmt.setString(3, passwordHash);
+
+            stmt.setString(4, "customer");
+            stmt.setInt(5, 0);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Could not connect to DB: ", e.getMessage());
+        }
+    }
+
 }
