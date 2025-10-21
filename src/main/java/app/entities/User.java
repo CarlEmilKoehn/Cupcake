3424d55c1f823398f1bf0cdb6b51fb3e1a4d8f60
olivermjmj <olivermjmj@outlook.dk>
2025-10-21 15:16:09 +0200

    added lombok to Maven and finished User entity.

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,6 +1,22 @@
 package app.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
 public class User {
 
+    private String email;
+    private String username;
+    private String password;
+    private String role;
 
+    private int balance;
+
+    @Override
+    public String toString() {
+        return email + " | " + username + " | " + role;
+    }
 }
