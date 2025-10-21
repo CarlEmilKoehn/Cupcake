e7c94dd0bde2b84016c554b12b17683a33577533
CarlEmilKoehn <carledk@live.dk>
2025-10-21 11:22:05 +0200

    Domænemodel og skelet opsat

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,31 @@
 package app;
 
-public class Main {
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import app.controllers.UserController;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main
+{
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+        app.get("/", ctx -> ctx.render("cupcake/index.html"));
+        //UserController.addRoutes(app);
+
+    }
 }
